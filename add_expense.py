82df41dc43de32f70c1a8ea1ae4e53995d678002
EssiRoberto <essiroberto@gmail.com>
2026-09-01 23:32:28 +0100

    second commit

--- add_expense.py
+++ add_expense.py
@@ -1,6 +1,6 @@
 expenses = []
 
-expenses.append({"item": "Groceries", "amount": 45.50})
+expenses.append({f"item": "Groceries", "amount": 45.50})
 expenses.append({"item": "Transport", "amount": 15.00})
 
 print("Total entries:", len(expenses))
